4c8e9cf2cc17ab970247d40d79751d6ea0d38bf2
kiljoonho <kiljh@me.com>
2019-10-01 15:19:54 +0900

    프로젝트 생성

---
+++ WikipediaSurfer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikipediaSurfer
+//
+//  Created by joon-ho kil on 10/1/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
