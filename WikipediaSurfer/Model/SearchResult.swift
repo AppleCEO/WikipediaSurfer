d49a08c5dd75f96b4b4b0a49dae41473d399037f
kiljoonho <kiljh@me.com>
2019-10-02 16:11:54 +0900

    위키피디아 API 호출 결과를 저장할 모델 생성

---
+++ WikipediaSurfer/Model/SearchResult.swift
@@ -0,0 +1,39 @@
+//
+//  SearchResult.swift
+//  WikipediaSurfer
+//
+//  Created by joon-ho kil on 10/1/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+enum SearchResultElement: Codable {
+    case string(String)
+    case stringArray([String])
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let x = try? container.decode([String].self) {
+            self = .stringArray(x)
+            return
+        }
+        if let x = try? container.decode(String.self) {
+            self = .string(x)
+            return
+        }
+        throw DecodingError.typeMismatch(SearchResultElement.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for SearchResultElement"))
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let x):
+            try container.encode(x)
+        case .stringArray(let x):
+            try container.encode(x)
+        }
+    }
+}
+
+typealias SearchResult = [SearchResultElement]
