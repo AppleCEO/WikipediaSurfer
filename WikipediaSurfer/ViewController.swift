08ef280614523f77d05b56807076ce2ed24c2ab1
kiljoonho <kiljh@me.com>
2019-10-04 07:54:25 +0900

    tableView 나오도록 구현

--- WikipediaSurfer/ViewController.swift
+++ WikipediaSurfer/ViewController.swift
@@ -12,20 +12,41 @@ import RxCocoa
 
 class ViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
-    var searchResult: Observable<SearchResult>?
+    @IBOutlet weak var tableView: UITableView!
     
+    var searchOb: Observable<SearchResult>?
+    var searchResult: SearchResult?
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        searchResult = JSONReceiver.getJson(search: "Hello world")
-        searchResult?
+        searchOb = JSONReceiver.getJson(search: "Hello world")
+        searchOb?
             .subscribe(onNext: { element in
-                dump(element)
+                self.searchResult = element
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             })
             .disposed(by: disposeBag)
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResult?.title.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = searchResult?.title[indexPath.row]
+        
+        return cell
     }
 }
